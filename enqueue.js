29e2b665a029aebdea1481cd97669455872c16ca
mcruz227 <121968692+mcruz227@users.noreply.github.com>
2024-03-19 22:25:48 -0400

    basics and youtube themes

--- enqueue.js
+++ enqueue.js
@@ -1,5 +1,24 @@
-async function fetchVideoData(){
+
+
+
+// code that gets the video data that the user added to their extension
+async function fetchVideoData(videoID){
+    const url = 'https://youtube138.p.rapidapi.com/video/details/?id=kJQP7kiw5Fk&hl=en&gl=US';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '211f148d38msh2540aed8b28799ep13bba9jsnf7f4e591a4c1',
+		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
 
 }
 
-fetchVideoData();
