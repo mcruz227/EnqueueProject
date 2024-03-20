b7f909ed9b799e21d863a54258d3a319cd74e2e9
YULIA EGORCHEVA <137467440+Comet8@users.noreply.github.com>
2024-03-19 21:18:56 -0400

    Update enqueue.js

--- enqueue.js
+++ enqueue.js
@@ -3,3 +3,12 @@ async function fetchVideoData(){
 }
 
 fetchVideoData();
+
+document.addEventListener("DOMContentLoaded", function() {
+  let sortBtn = document.querySelector('.sort-btn');
+  let sortOptions = document.querySelector('.sort-options');
+
+  sortBtn.addEventListener('click', function() {
+    sortOptions.classList.toggle('show');
+  });
+});
