78bc4da37451260613df21f1fbab559c4ace63d3
VAlva8 <victor080190@yahoo.com>
2024-03-21 03:39:10 -0400

    CSS Updates, and Minor HTML and JS changes
    CSS
    - Added the fonts used in the mockups
    - Increased the height and width of the extension by 25%
    - Change the color of the background to a darker, muted red
    - Changed the font size for the header title
    - Changed the background color of the sort button to transparent
    - Changed the font, margin, and padding of the sort button
    - Commented out the position attribute of the dropdown
    - Rounded the corners of the dropdown menu
    - Hid the overflow of the dropdown to maintain rounded corners
    - Center aligned the text of the dropdown menu
    
    HTML
    - Added a class to the header for specific handling
    - Added an arrow image meant to control the ascending and descending property of the videos
    - Put the sort by button and the arrow into a div container
    - Added two more buttons to the header to serve as Shuffle and Settings buttons
    
    JS
    - Added an eventListener function to control the ascending and descending of the arrow (So far just controls the appearance on click)
    - Added a global variable that states if the user wants the videos assorted by ascending or descending
    
    Files
    - Added the font files
    - Added three SVG files that are for the Sort arrow, shuffle button, and settings button

--- enqueue.js
+++ enqueue.js
@@ -1,5 +1,25 @@
+var ascending = false;
+
 async function fetchVideoData(){
 
 }
 
 fetchVideoData();
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("dropdown-order").addEventListener("click", flipOrder);
+  });
+
+function flipOrder(){
+    var dropdownOrder = document.getElementById("dropdown-order");
+
+    if(ascending){
+        dropdownOrder.style.transform = "rotate(0deg)";
+        dropdownOrder.title="Descending";
+        ascending = false;
+    }else{
+        dropdownOrder.style.transform = "rotate(180deg)";
+        dropdownOrder.title="Ascending";
+        ascending = true;
+    }
+}
