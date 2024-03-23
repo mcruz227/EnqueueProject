3157b4bf90f1e67973ed431453779b34329235bb
VAlva8 <victor080190@yahoo.com>
2024-03-23 17:43:36 -0400

    Added the Shell of Video Entries
    HTML
    - added the HTML for the video entries in the videoQueue '<div>'
    	- Container '<div>' for the entire video box
    	- '<h1>' tag responsible for the video's order number
    	- '<img>' tag responsible for holding the video's thumbnail
    	- Container '<div>' that hold's the video and channel title
    	- '<button>' for the drag feature
    - The HTML for the hard coded video entry is commented out for now
    - Added the images that served as buttons into a proper '<button>' tag for proper accessibility
    JS
    - Temporary button assignment to add a new video for test functions
    - Created the function that adds the shell of a video entry
    
    CSS
    - Added the CSS for the new HTML elements responsible for the video
    - Changed the scrollbar's appearance to match the material design of the extension
    - Removed the overflow attribute from the body to just the video container for a cleaner look
    - Changed the css of the new '<button>' tags to retain the same look they had when they were images

--- enqueue.js
+++ enqueue.js
@@ -1,4 +1,5 @@
 var ascending = false;
+var videos = 0;
 
 async function fetchVideoData(){
 
@@ -8,11 +9,12 @@ fetchVideoData();
 
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("dropdown-order").addEventListener("click", flipOrder);
-  });
+    document.getElementById("shuffle-button").addEventListener("click", addVideo);
+});
 
 function flipOrder(){
     var dropdownOrder = document.getElementById("dropdown-order");
-
+    
     if(ascending){
         dropdownOrder.style.transform = "rotate(0deg)";
         dropdownOrder.title="Descending";
@@ -22,4 +24,23 @@ function flipOrder(){
         dropdownOrder.title="Ascending";
         ascending = true;
     }
+}
+
+function addVideo(){
+    videos++;
+    document.getElementById("videoQueue").innerHTML+= "<div class='video-entry' id='entry" + videos + "'></div>";
+
+    var video = document.getElementById("entry" + videos);
+    video.innerHTML+="<h1 class='video-order-number' id='video-number-" + videos +"'>" + videos + "</h1>";
+    video.innerHTML+="<img class='video-thumbnail' id='video-image-" + videos + "'>";
+    video.innerHTML+="<div class='video-information-container' id='video-information-" + videos + "'></div>";
+
+    var informationContainer = document.getElementById("video-information-" + videos);
+    informationContainer.innerHTML+= "<h1 class='video-title' id='video-name-" + videos + "'>Video Title</h1>";
+    informationContainer.innerHTML+= "<p class='video-description' id='channel-name-" + videos + "'>Channel</p>";
+
+    video.innerHTML+= "<button class='image-button video-drag-button' id='video-" + videos + "-drag-button'></button>";
+
+    var dragButton = document.getElementById("video-" + videos + "-drag-button");
+    dragButton.innerHTML+= "<img src='./img/Drag.svg' id='video-drag-button-image'>";
 }
